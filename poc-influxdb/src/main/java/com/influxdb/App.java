8e1d37efd84acf536ab40a254bcb1db4392e6b02
Atul Sharma <atulkumarsharma1986@gmail.com>
2018-03-08 01:43:18 +0530

    checkin #1

---
+++ poc-influxdb/src/main/java/com/influxdb/App.java
@@ -0,0 +1,13 @@
+package com.influxdb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
